ca8a82f2a92564e8179f6d9789e80d3f568c15a4
P867-case <internetorethernet12@gmail.com>
2025-03-07 14:24:29 +0300

    Update flutter_bootstrap.js

--- TestOfApp/flutter_bootstrap.js
+++ TestOfApp/flutter_bootstrap.js
@@ -6,7 +6,7 @@ Read more: https://developer.mozilla.org/en-US/docs/Web/Security/Secure_Contexts
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e87","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e87","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"TestOfApp/main.dart.js"}]};
 
 
 _flutter.loader.load({
